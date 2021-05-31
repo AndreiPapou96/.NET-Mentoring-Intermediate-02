d39ad661d7905a1e23ff01abe1ceec71137d20df
a_papou <andrei.papou96@gmail.com>
2021-05-31 19:48:30 +0300

    Initial Commit

---
+++ AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using CloudServices.Interfaces;
+
+namespace AsyncAwait.Task2.CodeReviewChallenge.Models.Support
+{
+    public class ManualAssistant : IAssistant
+    {
+        private readonly ISupportService _supportService;
+
+        public ManualAssistant(ISupportService supportService)
+        {
+            _supportService = supportService ?? throw new ArgumentNullException(nameof(supportService));
+        }
+
+        public async Task<string> RequestAssistanceAsync(string requestInfo)
+        {
+            try
+            {
+                await _supportService.RegisterSupportRequestAsync(requestInfo);
+                var info = await _supportService.GetSupportInfoAsync(requestInfo);
+
+                return info;
+            }
+            catch (HttpRequestException ex)
+            {
+                return $"Failed to register assistance request. Please try later. {ex.Message}";
+            }
+        }
+    }
+}
